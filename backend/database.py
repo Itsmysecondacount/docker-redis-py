b154baf41f3d2d6d7e844340f218517387e8fe0c
Icarousjc <jorge.castillo@evol.biz>
2023-07-31 10:45:27 -0500

    asd

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,5 @@
 import redis
-from model import Timbre
-import json
+import json  # para convertir los objetos a JSON
 
 r = redis.Redis(host='redis', port=6379, db=0)
 
@@ -9,11 +8,14 @@ def fetch_all_todos():
     for key in r.keys('*'):
         value = r.get(key)
         value = value.decode("utf-8")
-        todos.append(value)
+        # debes decodificar los objetos JSON guardados en Redis
+        todo = json.loads(value)
+        todos.append(todo)
     return todos
 
 def create_todo(todo):
     key = f"timbre:{todo['datetime']}"
-    todo_str = str(todo)
+    # convierte el objeto todo a una cadena JSON antes de guardarla
+    todo_str = json.dumps(todo)
     r.set(key, todo_str)
     return todo
